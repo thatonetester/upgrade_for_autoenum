2a660ad666e14b5ab4dee3f1958235c4e0694fcb
Grimmz <57014148+thatonetester@users.noreply.github.com>
2020-03-14 18:54:18 -0400

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 echo "[*] Checking if anything requires updates, this may take a few minutes...."
-apt-get install nmap -y >> installing; rm installing &
-apt-get install nikto -y >> installing;rm installing &
-apt-get install wafw00f -y >> installing;rm installing &
-apt-get install gobuster -y >> installing;rm installing &
-apt-get install odat -y >> installing;rm installing &
-apt-get install oscanner -y >> installed;rm installing &
-apt-get install dnsenum -y >> installing; rm installing &
-apt-get install dnsrecon -y >> installing;rm installing &
-apt-get install fierce -y >> installing;rm installing &
-apt-get install onesixtyone -y >> installing;rm installing &
-apt-get install whatweb -y >> installing;rm installing &
-apt-get install rpcbind -y>> installing;rm installing &
+apt-get install nmap -y >> installing &
+apt-get install nikto -y >> installing &
+apt-get install wafw00f -y >> installing &
+apt-get install gobuster -y >> installing &
+apt-get install odat -y >> installing &
+apt-get install oscanner -y >> installing &
+apt-get install dnsenum -y >> installing &
+apt-get install dnsrecon -y >> installing &
+apt-get install fierce -y >> installing &
+apt-get install onesixtyone -y >> installing &
+apt-get install whatweb -y >> installing &
+apt-get install rpcbind -y >> installing &
 wait
+if [[ -f "installing" ]];then rm installing;fi
 echo "[*] Done!"
+
